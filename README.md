5a7ed9a219380cf7872ad222e664018b4c8bc60f
AlBan52 <balakin.nn@gmail.com>
2020-12-29 22:07:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Clicks count
+
+The present script - ```clicks_count.py``` could create the shorter website link than you have.
+Also, if the short link already exist, you could count total clicks quantity for this link. The
+getting clicks amount where for all time of using this link.
+
+### How to install
+
+Python3 should be already installed. 
+Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
+```
+pip install -r requirements.txt
+```
+
+### How to use
+
+For script running you have to start the command line and change directory to the code containing.
+The next step is start the script for this template: ```[full_dir_path] python click_count.py [full_link]```
+For example: ```d:\CODING\DEVMAN\Click_Count>python main.py https://dvmn.org/modules/web-api```
+
+### Output results
+
+As results you get shorten link, as name is bitlink. For example: ```Bitlink :  https://bit.ly/3pDYRyx```.
+Count clicks are outputs as for next example: ```Bitlinks clicks count: 0```
+If you want to count clicks amount on short link, you must start script for this template: ```[full_dir_path] python click_count.py [bitlink]```
+If you make mistake in link input, script get the following message: ```Incorrect link inputed. Please restart script```
+In this case, you need to restart script.
+
+### Project Goals
+
+The code is written for educational purposes on online-course for web-developers [dvmn.org](https://dvmn.org/).
---
+++ clicks_count.py
@@ -0,0 +1,68 @@
+import os
+import requests
+import urllib
+import argparse
+from dotenv import load_dotenv
+
+
+def parse_command_line():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('long_url', help='Original link')
+    args = parser.parse_args()
+    long_url = args.long_url
+    return long_url
+
+
+def shorten_link(long_url, headers):
+    long_url_data = {"long_url": long_url}
+    service_chapter = 'shorten'
+    response = requests.post(
+        f'https://api-ssl.bitly.com/v4/{service_chapter}',
+        headers=headers,
+        json=long_url_data)
+    response.raise_for_status()
+    response_data = response.json()
+    shorten_link = response_data.get('link')
+    return shorten_link
+
+
+def count_click(headers, bitlink):
+    parts_of_bitlink = urllib.parse.urlsplit(bitlink)
+    bitlink_for_response = f'{parts_of_bitlink.netloc}{parts_of_bitlink.path}'
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink_for_response}/clicks/summary',
+        headers=headers)
+    response.raise_for_status()
+    response_data = response.json()
+    total_clicks = response_data.get('total_clicks')
+    return total_clicks
+
+
+def check_link(headers, long_url):
+    service_chapter = 'bitlinks'
+    parts_of_bitlink = urllib.parse.urlsplit(long_url)
+    bitlink_for_response = f'{parts_of_bitlink.netloc}{parts_of_bitlink.path}'
+    response = requests.get(
+        f'https://api-ssl.bitly.com/v4/{service_chapter}/{bitlink_for_response}',
+        headers=headers)
+    return response.ok
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    token = os.getenv('GENERIC_ACCESS_TOKEN')
+    authorization = {'Authorization': f'Bearer {token}'}
+    long_url = parse_command_line()
+    check = check_link(authorization, long_url)
+    if check:
+        try:
+            clicks_count = count_click(authorization, long_url)
+            print('Bitlinks clicks count : ', clicks_count)
+        except requests.exceptions.HTTPError:
+            print('Incorrect link inputed. Please restart script')
+    else:
+        try:
+            bitlink = shorten_link(long_url, authorization)
+            print('Bitlink : ', bitlink)
+        except requests.exceptions.HTTPError:
+            print('Incorrect link inputed. Please restart script')
